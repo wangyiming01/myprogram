e75456af3b40c94ccd884778a6cb4b8a8e6b9f18
runoob <test@runoob.com>
2018-03-13 15:23:36 +0800

    添加了details界面的js功能

--- src/js/details.js
+++ src/js/details.js
@@ -1,5 +1,19 @@
 require(["/myProject/src/js/config.js"],function(){
 	require(["jquery","load","template","cookie"],function($,load,template,cookie){//用到的短名称
-		//要做的事情是什么的呢
+		var _amount=$("input",".amount").val();
+		$(".add").click(function(){			
+			_amount++;
+			console.log(_amount)
+			$("input",".amount").val(_amount);
+			return false;
+		})
+		var _amount=$("input",".amount").val();
+		$(".minus").click(function(){		
+			_amount--;		
+			if(_amount<=1)
+			_amount=1;
+			$("input",".amount").val(_amount);
+			return false;
+		})				
 	})
 })
